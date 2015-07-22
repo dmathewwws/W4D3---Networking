cd779daf21eabbf793a1cf02af4ad7b7c819f547
Daniel Mathews <dannyl.mathews@gmail.com>
2015-07-22 09:11:30 -0700

    Initial Commit

---
+++ W4D3 - Networking/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  W4D3 - Networking
+//
+//  Created by Daniel Mathews on 2015-07-22.
+//  Copyright (c) 2015 Daniel Mathews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
